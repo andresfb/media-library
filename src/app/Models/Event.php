3d8f6c953babcb46ede13f3058a37e395a309dc7
Andres Bastidas <andres@bastidas.us>
2022-09-30 23:32:59 -0400

    Project cleanup

--- src/app/Models/Event.php
+++ src/app/Models/Event.php
@@ -3,40 +3,11 @@
 namespace App\Models;
 
 use App\Traits\ConvertDateTimeToTimezone;
-use Eloquent;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Support\Carbon;
 
-/**
- * App\Models\Event
- *
- * @property int $id
- * @property int $item_id
- * @property string $action
- * @property string $requester
- * @property Carbon|null $deleted_at
- * @property Carbon|null $created_at
- * @property Carbon|null $updated_at
- * @property-read Item $item
- * @method static \Database\Factories\EventFactory factory(...$parameters)
- * @method static \Illuminate\Database\Eloquent\Builder|Event newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Event newQuery()
- * @method static \Illuminate\Database\Query\Builder|Event onlyTrashed()
- * @method static \Illuminate\Database\Eloquent\Builder|Event query()
- * @method static \Illuminate\Database\Eloquent\Builder|Event whereAction($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event whereDeletedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event whereItemId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event whereRequester($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Event whereUpdatedAt($value)
- * @method static \Illuminate\Database\Query\Builder|Event withTrashed()
- * @method static \Illuminate\Database\Query\Builder|Event withoutTrashed()
- * @mixin Eloquent
- */
 class Event extends Model
 {
     use HasFactory, SoftDeletes, ConvertDateTimeToTimezone;
