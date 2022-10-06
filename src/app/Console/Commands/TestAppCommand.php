95ba5aafbf5833c2bdfbe0a7bfdc18af4369e625
Andres Bastidas <andres@bastidas.us>
2022-10-06 18:56:28 -0400

    Added a package to read JSON files

--- src/app/Console/Commands/TestAppCommand.php
+++ src/app/Console/Commands/TestAppCommand.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use pcrov\JsonReader\JsonReader;
 
 class TestAppCommand extends Command
 {
@@ -21,6 +22,20 @@ class TestAppCommand extends Command
     {
         try {
             $this->info("Starting test");
+
+            $reader = new JsonReader();
+            $reader->open("/data1/media-library-dev/storage/app/upload/reddit_jokes.json");
+
+            $reader->read(); // Begin array
+            $reader->read(); // First element, or end of array
+            while($reader->type() === JsonReader::OBJECT) {
+                $data = $reader->value();
+                printf("%s: %s %s\n", $data["category"] ?? 'Reddit', $data["title"], $data["body"]);
+
+                $reader->next();
+            }
+
+            $reader->close();
 
             $this->newLine();
             $this->info("Done");
