5ef5068e4f188d6ac2f24548527e83e92a10fc1b
andresfb <andres@bastidas.us>
2022-10-14 21:09:16 -0400

    Added the schedule to run the ExtractExifJob

--- src/app/Console/Kernel.php
+++ src/app/Console/Kernel.php
@@ -2,8 +2,10 @@
 
 namespace App\Console;
 
+use App\Jobs\ExtractExifJob;
 use App\Jobs\GeneratePostJob;
 use App\Jobs\ImportMediaJob;
+use App\Services\ExtractExifService;
 use App\Services\ImportMediaService;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
@@ -18,6 +20,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
+        $schedule->job(new ExtractExifJob(new ExtractExifService()), 'media')
+            ->hourly();
+
         $schedule->job(new ImportMediaJob(new ImportMediaService()), 'ingestor')
             ->DailyAt("12:45");
 
